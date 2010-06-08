d9205daf74aeac43dfc9afb4b689ba4c40a2259e
Andre Steingress <andre.steingress@gmail.com>
2010-06-08 10:50:27 +0200

    refactoring: new PostconditionVisitor

--- src/main/java/org/gcontracts/injection/BasicAssertionInjector.java
+++ src/main/java/org/gcontracts/injection/BasicAssertionInjector.java
@@ -37,6 +37,7 @@ import org.gcontracts.annotations.Invariant;
 import org.gcontracts.annotations.Requires;
 import org.gcontracts.util.AnnotationUtils;
 import org.gcontracts.visitors.ClassInvariantVisitor;
+import org.gcontracts.visitors.PostconditionVisitor;
 import org.gcontracts.visitors.PreconditionVisitor;
 import org.objectweb.asm.Opcodes;
 
@@ -108,6 +109,7 @@ public class BasicAssertionInjector extends Injector {
 
                 final ClassInvariantVisitor classInvariantVisitor = new ClassInvariantVisitor(source);
                 final PreconditionVisitor preconditionVisitor = new PreconditionVisitor(source);
+                final PostconditionVisitor postconditionVisitor = new PostconditionVisitor(source);
 
                 List<AnnotationNode> annotations = method.getAnnotations();
                 for (AnnotationNode annotation: annotations)  {
@@ -117,7 +119,10 @@ public class BasicAssertionInjector extends Injector {
                         // fix compilation with setting value() to java.lang.Object.class
                         annotation.setMember(CLOSURE_ATTRIBUTE_NAME, new ClassExpression(ClassHelper.OBJECT_TYPE));
                     } else if (annotation.getClassNode().getName().equals(Ensures.class.getName()))  {
-                        generatePostconditionAssertionStatement(method, annotation);
+                        postconditionVisitor.generatePostconditionAssertionStatement(method, (ClosureExpression) annotation.getMember(CLOSURE_ATTRIBUTE_NAME));
+
+                        // fix compilation with setting value() to java.lang.Object.class
+                        annotation.setMember(CLOSURE_ATTRIBUTE_NAME, new ClassExpression(ClassHelper.OBJECT_TYPE));
                     }
                 }
 
@@ -132,113 +137,5 @@ public class BasicAssertionInjector extends Injector {
                 return null;
             }
         }.visitClass(classNode);
-    }
-
-    /**
-     * Injects a postcondition assertion statement in the given <tt>method</tt>, based on the given <tt>annotation</tt> of
-     * type {@link org.gcontracts.annotations.Ensures}.
-     *
-     * @param method the {@link org.codehaus.groovy.ast.MethodNode} for assertion injection
-     * @param annotation the {@link org.gcontracts.annotations.Ensures} annotation
-     */
-    public void generatePostconditionAssertionStatement(MethodNode method, AnnotationNode annotation)  {
-
-        // get the closure annotation
-        final ClosureExpression closureExpression = (ClosureExpression) annotation.getMember(CLOSURE_ATTRIBUTE_NAME);
-        // fix compilation with setting value() to java.lang.Object.class
-        annotation.setMember(CLOSURE_ATTRIBUTE_NAME, new ClassExpression(ClassHelper.OBJECT_TYPE));
-
-        // check whether the closure uses a result or old variable
-        boolean usesResultVariable = false;
-        boolean usesOldVariable = false;
-        boolean usesResultVariableFirst = false;
-
-        for (Parameter closureParameter : closureExpression.getParameters())  {
-            if (closureParameter.getName().equals("old"))  {
-                usesOldVariable = true;
-            } else if (closureParameter.getName().equals("result"))  {
-                usesResultVariable = true;
-                usesResultVariableFirst = !usesOldVariable;
-            }
-        }
-
-        MapExpression oldVariableMap = new MapExpression();
-
-        if (usesOldVariable)  oldVariableMap = new VariableGenerator().generateOldVariablesMap(method);
-
-        final BlockStatement methodBlock = (BlockStatement) method.getCode();
-
-        // if return type is not void, than a "result" variable is provided in the postcondition expression
-        final List<Statement> statements = methodBlock.getStatements();
-        if (statements.size() > 0)  {
-            BlockStatement postconditionCheck = null;
-
-            if (method.getReturnType() != ClassHelper.VOID_TYPE && usesResultVariable)  {
-                Statement lastStatement = statements.get(statements.size() - 1);
-
-                ReturnStatement returnStatement = getReturnStatement(lastStatement);
-
-                statements.remove(statements.size() - 1);
-
-                postconditionCheck = AssertStatementCreator.getAssertionBlockStatement(method, closureExpression, "postcondition", closureToSourceConverter.convertClosureExpressionToSourceCode(closureExpression, source));
-
-                // Assign the return statement expression to a local variable of type Object
-                VariableExpression resultVariable = new VariableExpression("result");
-                ExpressionStatement resultVariableStatement = new ExpressionStatement(
-                new DeclarationExpression(resultVariable,
-                        Token.newSymbol(Types.ASSIGN, -1, -1),
-                        returnStatement.getExpression()));
-
-                postconditionCheck.getStatements().add(0, resultVariableStatement);
-
-                // Assign the return statement expression to a local variable of type Object
-                VariableExpression oldVariable = new VariableExpression("old");
-                ExpressionStatement oldVariabeStatement = new ExpressionStatement(
-                new DeclarationExpression(oldVariable,
-                        Token.newSymbol(Types.ASSIGN, -1, -1),
-                        oldVariableMap));
-
-                postconditionCheck.getStatements().add(0, oldVariabeStatement);
-                
-                methodBlock.addStatement(postconditionCheck);
-                methodBlock.addStatement(returnStatement);
-            } else {
-
-                postconditionCheck = AssertStatementCreator.getAssertionBlockStatement(method, closureExpression, "postcondition", closureToSourceConverter.convertClosureExpressionToSourceCode(closureExpression, source));
-
-                // Assign the return statement expression to a local variable of type Object
-                VariableExpression oldVariable = new VariableExpression("old");
-                ExpressionStatement oldVariabeStatement = new ExpressionStatement(
-                new DeclarationExpression(oldVariable,
-                        Token.newSymbol(Types.ASSIGN, -1, -1),
-                        oldVariableMap));
-
-                postconditionCheck.getStatements().add(0, oldVariabeStatement);
-
-                methodBlock.addStatement(postconditionCheck);
-            }
-        }
-    }
-
-    /**
-     * Gets a {@link org.codehaus.groovy.ast.stmt.ReturnStatement} from the given {@link org.codehaus.groovy.ast.stmt.Statement}.
-     *
-     * @param lastStatement the last {@link org.codehaus.groovy.ast.stmt.Statement} of some method code block
-     * @return a {@link org.codehaus.groovy.ast.stmt.ReturnStatement} or <tt>null</tt>
-     */
-    private ReturnStatement getReturnStatement(Statement lastStatement)  {
-
-        if (lastStatement instanceof ReturnStatement)  {
-            return (ReturnStatement) lastStatement;
-        } else if (lastStatement instanceof BlockStatement) {
-            BlockStatement blockStatement = (BlockStatement) lastStatement;
-            List<Statement> statements = blockStatement.getStatements();
-
-            return statements.size() > 0 ? getReturnStatement(statements.get(statements.size() - 1)) : null;
-        } else {
-            // the last statement in a Groovy method could also be an expression which result is treated as return value
-            ExpressionStatement expressionStatement = (ExpressionStatement) lastStatement;
-            return new ReturnStatement(expressionStatement);
-        }
     }
 }
