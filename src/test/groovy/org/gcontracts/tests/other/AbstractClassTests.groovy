b57ebf9826a026e3bd2b238b99b5843b2e8cab52
Andre Steingress <andre.steingress@gmail.com>
2010-08-05 09:28:34 +0200

    new abstract class test cases

---
+++ src/test/groovy/org/gcontracts/tests/other/AbstractClassTests.groovy
@@ -0,0 +1,65 @@
+package org.gcontracts.tests.other
+
+import org.gcontracts.tests.basic.BaseTestClass
+
+/**
+ * @author andre.steingress@gmail.com
+ */
+class AbstractClassTests extends BaseTestClass {
+
+  def source1 = '''
+package tests
+
+import org.gcontracts.annotations.*
+
+@Invariant({ property != null })
+abstract class A {
+
+  def property
+
+  def A(def someValue)  {
+    property = someValue
+  }
+
+  @Requires({ param != null })
+  def some_operation(def param)  {
+    // noop
+  }
+}
+'''
+
+  def source2 = '''
+package tests
+
+import org.gcontracts.annotations.*
+
+class B extends A  {
+
+  def B(def someValue)  {
+    super(someValue)
+  }
+
+  def some_operation(def param)  {
+    // noop
+  }
+}
+'''
+
+  def void test_inherited_class_invariant()  {
+    add_class_to_classpath source1
+
+    shouldFail AssertionError, {
+       create_instance_of(source2, [null])
+    }
+  }
+
+  def void test_inherited_precondition()  {
+    add_class_to_classpath source1
+
+    shouldFail AssertionError, {
+       def bInstance = create_instance_of(source2, ["test"])
+       bInstance.some_operation null
+    }
+  }
+
+}
