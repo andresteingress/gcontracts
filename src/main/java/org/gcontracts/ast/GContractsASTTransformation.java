2564ed31f16ba56ef8b9d5fa4d49328d6275b6e8
Andre Steingress <me@andresteingress.com>
2011-02-24 14:58:14 +0100

    Groovy 1.7.8, cleaned up debug statement

--- src/main/java/org/gcontracts/ast/GContractsASTTransformation.java
+++ src/main/java/org/gcontracts/ast/GContractsASTTransformation.java
@@ -29,6 +29,7 @@ import org.codehaus.groovy.control.CompilePhase;
 import org.codehaus.groovy.control.SourceUnit;
 import org.codehaus.groovy.control.io.ReaderSource;
 import org.codehaus.groovy.transform.GroovyASTTransformation;
+import org.gcontracts.ast.visitor.ContractsErasingVisitor;
 import org.gcontracts.ast.visitor.ContractsVisitor;
 import org.gcontracts.ast.visitor.DynamicSetterInjectionVisitor;
 
@@ -60,6 +61,7 @@ public class GContractsASTTransformation extends BaseASTTransformation {
         for (final ClassNode classNode : moduleNode.getClasses())  {
             new ContractsVisitor(unit, source).visitClass(classNode);
             new DynamicSetterInjectionVisitor(unit, source).visitClass(classNode);
+            new ContractsErasingVisitor(unit, source).visitClass(classNode);
         }
     }
 }
