b7b35a639628b36cc7a53ade2599ac932109fee1
Andre Steingress <me@andresteingress.com>
2013-03-15 13:25:07 +0100

    fixed test-case

--- gcontracts-core/src/test/groovy/org/gcontracts/generation/ContractExecutionTrackerTests.groovy
+++ gcontracts-core/src/test/groovy/org/gcontracts/generation/ContractExecutionTrackerTests.groovy
@@ -9,6 +9,8 @@ class ContractExecutionTrackerTests {
 
     @Test void track_double_execution() {
 
+        ContractExecutionTracker.clear('Dummy', 'method 1', 'pre', false)
+
         assert ContractExecutionTracker.track('Dummy', 'method 1', 'pre', false)
         assert ContractExecutionTracker.track('Dummy', 'method 1', 'pre', false) == false
 
@@ -16,6 +18,9 @@ class ContractExecutionTrackerTests {
     }
 
     @Test void clear_only_for_first_stack_element() {
+
+        ContractExecutionTracker.clear('Dummy', 'method 1', 'pre', false)
+        ContractExecutionTracker.clear('Dummy', 'method 2', 'pre', false)
 
         assert ContractExecutionTracker.track('Dummy', 'method 1', 'pre', false)
         assert ContractExecutionTracker.track('Dummy', 'method 2', 'pre', false)
@@ -29,6 +34,8 @@ class ContractExecutionTrackerTests {
     }
 
     @Test void track_static_method() {
+
+        ContractExecutionTracker.clear('Dummy', 'method 1', 'pre', false)
 
         assert ContractExecutionTracker.track('Dummy', 'method 1', 'pre', true)
         assert ContractExecutionTracker.track('Dummy', 'method 1', 'pre', true) == false
